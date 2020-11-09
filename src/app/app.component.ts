a3711ad0e20d3bc0f963ba14d06f4845d2e5ef81
Kuni <kuni.ujiie@grapecity.com>
2020-11-09 13:23:14 +0900

    add wijmo flexgrid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wijmo-angular10-app';
+
+  gridData = [
+    { id: 15, product: 'ピュアデミグラスソース', date: '2017/01/10', amount: 6000 },
+    { id: 17, product: 'だしこんぶ', date: '2017/01/08', amount: 14500 },
+    { id: 18, product: 'ピリカラタバスコ', date: '2017/01/12', amount: 4000 },
+    { id: 84, product: 'なまわさび', date: '2017/01/21', amount: 8000 }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import '@grapecity/wijmo.cultures/wijmo.culture.ja';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WjGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
